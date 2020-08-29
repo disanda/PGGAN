8a902e19937a52a831495a76630cd41a33439590
disanda <skyanda1@qq.com>
2020-08-29 22:30:18 +0800

    x

--- pro_gan_pytorch/Pro_GAN_Again.py
+++ pro_gan_pytorch/Pro_GAN_Again.py
@@ -41,12 +41,14 @@ def update_average(model_tgt, model_src, beta):
     toggle_grad(model_src, True)
 
 device= 'cuda'
-#netG = torch.nn.DataParallel(net.Generator(depth=9,latent_size=1024))# in: [-1,512], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
-netG = net.Generator(depth=9,latent_size=1024)
-netG.load_state_dict(torch.load('./result/pre-model/GAN_GEN_SHADOW_3.pth',map_location=device)) #shadow的效果要好一些 
-#netD = torch.nn.DataParallel(net.Discriminator(height=9, feature_size=1024))# in: [-1,3,1024,1024],out:[], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
-netD = net.Discriminator(height=9, feature_size=1024)
-netD.load_state_dict(torch.load('./result/pre-model/GAN_DIS_3.pth',map_location=device))
+netG = torch.nn.DataParallel(net.Generator(depth=9,latent_size=1024))# in: [-1,512], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
+#netG.load_state_dict(torch.load('./result/pre-model/GAN_GEN_SHADOW_3.pth',map_location=device)) #shadow的效果要好一些 
+netG.load_state_dict({k.replace('module.',''):v for k,v in torch.load('./result/pre-model/GAN_GEN_SHADOW_3.pth').items()},map_location=device)
+
+
+netD = torch.nn.DataParallel(net.Discriminator(height=9, feature_size=1024))# in: [-1,3,1024,1024],out:[], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
+#netD.load_state_dict(torch.load('./result/pre-model/GAN_DIS_3.pth',map_location=device))
+netD.load_state_dict({k.replace('module.',''):v for k,v in torch.load('./result/pre-model/GAN_DIS_3.pth').items()},map_location=device)
 
 # ProGAN Module (Unconditional)
 class ProGAN:
