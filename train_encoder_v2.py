5eeceffb59949f3d3c8ec6ee6f0119cf25f47d03
disanda <skyanda1@qq.com>
2020-09-03 10:55:22 +0800

    x

--- train_encoder_v2.py
+++ train_encoder_v2.py
@@ -66,7 +66,7 @@ def toggle_grad(model, requires_grad):
         p.requires_grad_(requires_grad)
 
 netG = torch.nn.DataParallel(net.Generator(depth=9,latent_size=512))# in: [-1,512], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
-#netG.load_state_dict(torch.load('./pre-model/GAN_GEN_SHADOW_8.pth',map_location=device)) #shadow的效果要好一些 
+netG.load_state_dict(torch.load('./pre-model/GAN_GEN_SHADOW_8.pth',map_location=device)) #shadow的效果要好一些 
 #netD1 = torch.nn.DataParallel(net.Discriminator(height=9, feature_size=512))# in: [-1,3,1024,1024],out:[], depth:0-4,1-8,2-16,3-32,4-64,5-128,6-256,7-512,8-1024
 #netD1.load_state_dict(torch.load('./pre-model/GAN_DIS_8.pth',map_location=device))
 
