61f98e57bd3834ed9819c6194f8a9781b1109f05
disanda <skyanda1@qq.com>
2020-09-13 22:43:04 +0800

    x

--- train_celeba1024.py
+++ train_celeba1024.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     # hyper-parameters per depth (resolution)
     num_epochs = [10, 10, 10, 10, 8, 8,8 ,8, 8]
     fade_ins = [100, 90, 80, 70, 60, 50, 40, 30, 20]
-    batch_sizes = [128, 128, 128, 64, 32, 16, 8, 6, 4]
+    batch_sizes = [128, 128, 128, 64, 32, 16, 8, 6, 3] # 1024不能超过3
     latent_size = 1024
 
 
